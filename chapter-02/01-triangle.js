ac11d4d6e6455850e5031e6a2e1a8c6d9a98ea32
Tiago Ilieve <tiago.myhro@gmail.com>
2018-04-13 16:00:19 -0300

    Add 'chapter-02/'

---
+++ chapter-02/01-triangle.js
@@ -0,0 +1,5 @@
+let t = '#';
+for (let i = 0; i < 7; i++) {
+  console.log(t);
+  t += '#';
+}
---
+++ chapter-02/03-chess-board.js
@@ -0,0 +1,13 @@
+let board = '';
+let size = 8;
+for (let i = 0; i < size; i++) {
+  for (let j = 0; j < size; j++) {
+    if ((i + j) % 2 == 0) {
+      board += ' ';
+    } else {
+      board += '#';
+    }
+  }
+  board += '\n';
+}
+console.log(board);
